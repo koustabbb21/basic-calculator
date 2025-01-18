87b0e01430192b679abb9c15e5bb9ec02abbe61e
Koustab Saikia <Koustavsaikia63@gmail.com>
2025-01-18 22:10:39 +0530

    Added the sum function

--- main.c
+++ main.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 
+void sum();
+
 int main()
 {
     char op;
     printf("Enter an operator(+,-,*,/) : ");
     scanf("%c", &op);
+
+    if(op=='+')
+    {
+        sum();
+    }
     return 0;
+}
+
+void sum()
+{
+    int a, b;
+    printf("Enter two numbers : ");
+    scanf("%d %d", &a, &b);
+    printf("Sum of %d and %d is %d\n",a,b, a+b);
 }
