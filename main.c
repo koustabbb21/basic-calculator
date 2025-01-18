6d75c87a410a359a2901f6662a71f37215938723
Koustab Saikia <Koustavsaikia63@gmail.com>
2025-01-18 22:18:45 +0530

    Added subt, mult and div functions

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 void sum();
+void subt();
+void mult();
+void div();
 
 int main()
 {
@@ -21,4 +24,28 @@ void sum()
     printf("Enter two numbers : ");
     scanf("%d %d", &a, &b);
     printf("Sum of %d and %d is %d\n",a,b, a+b);
+}
+
+void subt()
+{
+    int a, b;
+    printf("Enter two numbers : ");
+    scanf("%d %d", &a, &b);
+    printf("Sum of %d and %d is %d\n",a,b, a-b);
+}
+
+void mult()
+{
+    int a, b;
+    printf("Enter two numbers : ");
+    scanf("%d %d", &a, &b);
+    printf("Sum of %d and %d is %d\n",a,b, a*b);
+}
+
+void div()
+{
+    int a, b;
+    printf("Enter two numbers(b>0): ");
+    scanf("%d %d", &a, &b);
+    printf("Sum of %d and %d is %d\n",a,b, a/b);
 }
